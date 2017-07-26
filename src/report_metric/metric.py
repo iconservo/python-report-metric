2600c578d09df3cb5d4309c6cfeae270a8d36e93
Steven Reid <res@ciklum.com>
2017-07-26 16:55:11 -0700

    Reworking source/destination handling

--- src/report_metric/metric.py
+++ src/report_metric/metric.py
@@ -6,12 +6,15 @@ from celery.task import task
 
 logging.basicConfig(level=logging.DEBUG)
 
-def setup_reporter(destination = None):
+
+def setup_reporter(destination=None, source=None):
     destination = destination or settings.get('METRICS_DESTINATION', 'librato')
+    source      = source or settings.get('METRICS_SOURCE', None)
+
     if destination == 'librato' and reporter.LIBRATO and settings.get('METRICS_LIBRATO_USER'):
         return reporter.LibratoReport(username=settings.get('METRICS_LIBRATO_USER'),
                                       api_key=settings.get('METRICS_LIBRATO_TOKEN'),
-                                      source=settings.get('METRICS_LIBRATO_SOURCE', None))
+                                      source=source)
 
     elif destination == 'collectd' and reporter.COLLECTD:
         return reporter.CollectdReport()
@@ -19,7 +22,8 @@ def setup_reporter(destination = None):
         return reporter.DirectReport()
     raise reporter.StatsReportException('No available/configured destination') # maybe not right exception
 
-def gauge(name, number, destination=None):
+
+def gauge(name, number, **kwargs):
     '''
     Helper method for single call sending of a gauge
     :param name: metric name
@@ -28,11 +32,12 @@ def gauge(name, number, destination=None):
     :return: 
     '''
     if settings.get('METRICS_USE_CELERY', False):
-        _report_gauge.delay(name, number, destination)
+        _report_gauge.delay(name, number, **kwargs)
     else:
-        _report_gauge(name, number, destination)
+        _report_gauge(name, number, **kwargs)
 
-def counter(name, number=1, destination=None):
+
+def counter(name, number=1, **kwargs):
     '''
     Helper method for single call sending of a counter
     :param name: metric name
@@ -41,22 +46,24 @@ def counter(name, number=1, destination=None):
     :return:
     '''
     if settings.get('METRICS_USE_CELERY', False):
-        _report_counter.delay(name, number, destination)
+        _report_counter.delay(name, number, **kwargs)
     else:
-        _report_counter(name, number, destination)
+        _report_counter(name, number, **kwargs)
 
 
 # There's got to be a more elegant way to conditionally wrap a function in Task, but for the moment
 @task(name='report_metric.gauge')
-def _report_gauge(name, number, destination=None):
+def _report_gauge(name, number, **kwargs):
     try:
-        setup_reporter(destination).gauge(name, number)
+        rep = setup_reporter(kwargs.get('destination', None), kwargs.get('source', None))
+        rep.gauge(name, number)
     except reporter.StatsReportException as e:
         logging.exception(str(e))
 
+
 @task(name='report_metric.counter')
-def _report_counter(name, number=1, destination=None):
+def _report_counter(name, number=1, **kwargs):
     try:
-        setup_reporter(destination).counter(name, number)
+        setup_reporter(kwargs.get('destination', None), kwargs.get('source', None)).counter(name, number)
     except reporter.StatsReportException as e:
         logging.exception(str(e))
